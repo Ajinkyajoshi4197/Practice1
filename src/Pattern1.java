3499639f8096c2c950be384a6f0d0502b5f4e81a
Ajinkyajoshi4197 <121416381+Ajinkyajoshi4197@users.noreply.github.com>
2022-12-26 14:16:29 +0530

    commiting first project

---
+++ src/Pattern1.java
@@ -0,0 +1,17 @@
+
+
+	public class Pattern1{
+	    public static void main (String[] args)
+	    {
+	        int i,j,n=5;
+	        for(i=n;i>0;i--){
+	            for(j=1;j<=i;j++)
+	            {
+	                System.out.print("*");
+	            }
+	            System.out.println("");
+	        }
+	    }
+	}
+
+
